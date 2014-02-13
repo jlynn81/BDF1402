53a372608cacf34c4529c73eabcd5b97456ed864
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-13 00:29:02 -0600

    File Update
    Removed a previous .php file that wasn’t needed
    Added the database.php to the MVC-READ File

--- MVC-Read/index.php
+++ MVC-Read/index.php
@@ -1,12 +1,41 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: jlynn81
+ * Class BDF1402
  * Date: 2/10/14
- * Time: 9:25 PM
  */
 
-echo "Hello World. Today is ".date("Y/m/d");
+    // Creates Index Page
+include 'models/viewModel.php';
+include 'models/contactsModel.php';
+
+$pagename = 'index';
+
+$views = new viewModel();
+$contacts = new contactsModel();
+
+    //Display Header with buttons
+
+$views->getView("views/header.inc");
+
+    //Create beginning of User List
+if(!empty($_GET["action"])){
+    if($_GET["action"]=="home"){
+        $result = $contacts->getAll();
+        $views->getView("views/body.php",$result);
+    }if($_GET["action"]=="details"){
+        $result = $contacts->getOne($_GET["id"]);
+        $views->getView("views/details.php",$result);
+    }
+}else{
+    $result = $contacts->getALL();
+    $views->getView("views/body.php",$result);
+}
+
+    //Display page Footer
+$views->getView("views/footer.inc");
+
+
 
 
 ?>
