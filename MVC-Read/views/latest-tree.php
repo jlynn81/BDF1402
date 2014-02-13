82c21adfafbdb06cf2b461287a2ecf1f9a1d55cd
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-13 07:38:04 -0600

    Add CSS Stylization

--- MVC-Read/views/latest-tree.php
+++ MVC-Read/views/latest-tree.php
@@ -4,7 +4,8 @@ $lastTree = '';
 
 foreach ($rows as $num => $row){
     if($lastTree !== $row['Height']) {
-        echo "<h2>${row['Name']}: ${row['Height']}</h2>";
+        echo "<h2>${row['Name']}:</h2>";
+        echo "<h3>${row['Height']}</h3>";
     }
     echo "<li>${row['Description']}</li>";
     $lastTree = $row['Height'];
