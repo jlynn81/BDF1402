ad16493d1a5f666112e9fde88fd8789e79a93b2c
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-21 21:57:27 -0600

    updated all files for CRUD

--- CRUD/models/treeModel.php
+++ CRUD/models/treeModel.php
@@ -51,4 +51,30 @@ class treeModel extends DB{
             $_SESSION["successful"] = 0;
         }
 
-} 
+        public function update($id=0, $height='', $description=''){
+
+            $sql = "update tree_details set height = :height, description = :description where id = :id";
+            $st = $this->db->prepare($sql);
+            $st->execute(array(":id"=>$id, ":height"=>$height, ":description" => $description));
+        }
+
+    public function delete($id=0){
+        $sql = "delete from tree_details where id =:id";
+        $st = $this->db->prepare($sql);
+        $st->execute(array(":id"=>$id));
+
+        $sql = "delete from TREES where id = :id";
+        $st = $this->db->prepare($sql);
+        $st->execute(array(":id"=>$id));
+    }
+
+    public function add( $user_name = '', $user_password='', $NAME = '', $height ='', $description = ''){
+
+        $sql = "insert into TREES (user_name, user_password, NAME, height, description)
+							values (  :user_name, :user_password, :NAME, :height, :description)";
+        $st = $this->db->prepare($sql);
+        $st-> execute(array(":user_name"=> $user_name, ":user_password" => $user_password, ":NAME" => $NAME, ":height" => $height, ":description"=>$description));
+        
+    }
+
+}
--- CRUD/protected.php
+++ CRUD/protected.php
@@ -11,14 +11,61 @@ $pagename = 'protected';
 $views = new treeModel();
 $trees = new treeViewModel();
 
+//Display header
 $views->getView("views/header.inc");
 
 //Show initial list
 if(!empty($_GET["action"])){
 
+    if($_GET["action"]=="update"){
+
+        $result = $trees->getOne($_GET["id"]);
+        $views->getView("views/updateform.html", $result);
+
+    }else if($_GET["action"]=="updateaction"){
+
+        $contacts->update($_GET["id"], $_POST["height"], $_POST["description"]);
+        $result = $contacts->getAll();
+        $views->getView("views/protected.php",$result);
+
+    }else if($_GET["action"] == "delete"){
+
+        $contacts->delete($_GET["id"]);
+        $result = $contacts->getAll();
+        $views->getView("views/protected.php",$result);
+
+    }else if ($_GET["action"] == "add"){
+
+        $views->getView("views/addform.html");
+
+    }else if ($_GET["action"] == "addaction"){
+        $contacts->add($_POST["user_name"], $_POST["user_password"],  $_POST["NAME"], $_POST["height"], $_POST["description"] ); //
+        $result = $contacts -> getAll();
+        $views->getView("views/protected.php", $result);
+    }
+
+
+    //if($_GET["action"]=="home"){
+    //	$result = $trees->getAll();
+    //	$views->getView("views/tree.php", $result);
+    //}
+
+    //if($_GET["action"]=="details"){
+    //	$result=$trees->getOne($_GET["id"]);
+    //	$views->getView("views/treeDetails.php",$result);
+    //$data = $trees->getAll();
+    //$views->getView("views/tree.php",$data);
+
+
 }else{
-    //Protected Data and CRUD Information
+
+    $result = $trees->getAll();
+    $views->getView("views/protected.php", $result);
+
+    //$data = $trees->getAll();
+
+    //$views->getView("views/tree.php",$data);
 }
 
-//Show Footer
 $views->getView("views/footer.inc");
+?>
