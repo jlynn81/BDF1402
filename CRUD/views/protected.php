7a72e3b053af83f7c60f73340cee41a5d6c8c02b
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-23 19:27:35 -0600

    Updated CSS Style
    Added styling for the website to look better
    Added respective tags to elements to allow for CSS

--- CRUD/views/protected.php
+++ CRUD/views/protected.php
@@ -4,19 +4,26 @@
 //protected data view
 
 foreach($data as $d){
+    echo "<table>";
     echo " <b>Name: </b>";
     echo $d["name"];
+    echo "<br>";
 //    echo " <b>User Name:</b>";
 //    echo $d["user_name"];
 //    echo "<b>User Password</b>";
 //    echo $d["user_password"];
-    echo " <b>Tree Height: </b> ";
+    echo " <b>Height: </b> ";
     echo $d["height"];
-    echo " <b>Tree Description: </b> ";
-    echo $d["description"];
-    echo " <a href='?action=update&id=".$d["id"]."'>Update</a>";
-    echo " <a href='?action=delete&id=".$d["id"]."'>Delete</a>";
     echo "<br>";
+
+    echo " <b>Description: </b> ";
+    echo $d["description"];
+    echo "<br>";
+
+    echo " <a href='?action=update&id=".$d["id"]."'>Update</a>";
+    echo "<break>";
+    echo " <a href='?action=delete&id=".$d["id"]."'>Delete</a>";
+    echo "</table>";
 }
 ?>
 </div>
