1d381a6ffd82f77be45660740cc5cd35e5d1cdf7
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-26 17:29:40 -0600

    Working on Error Message

--- Framework/CodeIgniter/application/views/welcome_message.php
+++ Framework/CodeIgniter/application/views/welcome_message.php
@@ -2,7 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Welcome to a Tree Database</title>
 
 	<style type="text/css">
 
