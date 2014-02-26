31ea335b2f79aaf377d523221494984389d3ba10
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-26 04:52:04 -0600

    Added CodeIgniter for my framework
    Added respective files for the framework to be utilized

---
+++ Framework/CodeIgniter_2.1.4/application/views/Hello.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<header>
+    <meta charset="UTF-8"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+    <!--    Mobile viewport optimized: j.mp/bplateviewport-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+
+    <!--    CSS site stylization-->
+<!--    <link rel="stylesheet" type="text/css" href="CSS/styles.css"/>-->
+
+    <!--    Web font for site-->
+    <link href='http://fonts.googleapis.com/css?family=Ledger' rel='stylesheet' type='text/css'>
+
+    <title>Plants</title>
+
+</header>
+
+<body>
+
+<h1>Tree Types</h1>
+
+<?php
+echo "<p>Hello World</p>";
+echo date('l jS \of F Y h:i:s A');
+?>
+
+</body>
+
+</html>
