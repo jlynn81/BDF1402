57dcb1f8fb7624693c22e5d71b9013c4cca5ec45
Jennifer L Brown <jlynn81@fullsail.edu>
2014-02-26 20:12:25 -0600

    Working on layout, Still have Error Message

--- Framework/CodeIgniter/application/views/tree_details.php
+++ Framework/CodeIgniter/application/views/tree_details.php
@@ -1,13 +1,19 @@
+<div id="treeDisc">
+
 <?php
 
-echo "<center>";
+//echo "<center>";
 foreach($data as $d){
 
+    echo "<b>Name:</b> ";
+    echo $d["name"];
     echo " <b>Height:</b> ";
     echo $d["height"];
     echo " <b>Description:</b> ";
     echo $d["description"];
     echo "<br>";
 }
-echo "</center>";
+//echo "</center>";
 ?>
+
+</div>
